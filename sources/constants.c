859a103d745677c4bfb0a5548ed7da4421a5086d
Xin Wang <dram.wang@gmail.com>
2018-04-22 14:21:45 +0800

    Constants should be declared with parameter attribute.

--- sources/constants.c
+++ sources/constants.c
@@ -1,9 +1,11 @@
 #include <apr_errno.h>
 #include <stdio.h>
 
+#define INT_PARAMETER "integer(c_int), parameter :: "
+
 int main(void)
 {
-	printf("integer(c_int) :: apr_success = %d\n", APR_SUCCESS);
+	printf(INT_PARAMETER "apr_success = %d\n", APR_SUCCESS);
 
 	return 0;
 }
